fb4dca4e0f9b4aa80344cec0ca33a967ff4829c6
meron37 <134938986+meron37@users.noreply.github.com>
2025-10-18 14:48:00 -0500

    I have added exercise 1

---
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+    int count = 1;
+    do{
+        System.out.println(" i love java");
+        count++;
+    }
+ while (count <= 5);
+
+
+    }
+
+}
